6517cdaf6484a4ade44744e58f2f78dc58a2aa80
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-12-20 20:31:00 -0600

    added handle submit

--- EmailSite/src/components/EmailForm.tsx
+++ EmailSite/src/components/EmailForm.tsx
@@ -2,25 +2,35 @@ import React, { useEffect, useState } from 'react'
 import { Email } from '../app/email'
 import { apiCall } from '../app/lib'
 
+interface IError {
+    display: boolean,
+    message: string
+}
+
 const EmailForm = () => {
     let [data, setData] = useState<Email>({
         Recipient: "",
         Subject: "",
         Body: ""
     })
+    let [error, setError] = useState<IError>({display: false, message: ''})
 
-    async function handleSubmit(e: React.SyntheticEvent) {
+    function handleSubmit(e: React.SyntheticEvent) {
         e.preventDefault()
         apiCall("POST", data)
+            .then(res => res.json())
             .then(response => {
-                if(response.ok) {
+                if(response.Status) {
                     setData({
                         Recipient: "",
                         Subject: "",
                         Body: "",
                     })
-                }
-            })
+                }else{
+                    setError({display: true, message: response.Message})
+                }})
+            .catch(e => console.log(e))
+            
     }
   return (
     <div>EmailForm</div>
